a758296df2cd74b39c40b610353de474c65fee07
Shogo <shogo.a.hardy@gmail.com>
2026-04-01 10:14:18 -0600

    Add React app with name and passing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders my name on the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/Shogo Hardy/i);
+  expect(nameElement).toBeInTheDocument();
 });
